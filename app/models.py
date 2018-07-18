5d27f0b52c93db9df18243e67adc3011626ff503
prescottbreeden <prescott.breeden@gmail.com>
2018-07-18 02:30:51 -0700

    building database for tech
    sort of overkill but good practice

--- app/models.py
+++ app/models.py
@@ -1,16 +1,15 @@
-from sqlalchemy import Column, Integer, String
-from portfolio.database import Base
+from app import db
 
 
-class Tech(Base):
+class Tech(db.Model):
     __tablename__ = 'tech'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50), unique=True)
-    blurb = Column(String(255), unique=True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+    blurb = db.Column(db.String(255), unique=True)
 
     def __init__(self, name=None, blurb=None):
         self.name = name
         self.blurb = blurb
 
     def __repr__(self):
-        return '<Tech %r>' % (self.name)
+        return '<Tech {}>'.format(self.name)

37dad3a59b7465b639bbbfdf12969b764a3ce50a
prescottbreeden <prescott.breeden@gmail.com>
2018-07-17 20:25:54 -0700

    waypoints trigger sticky and scroll to
    set up content container to use js to swap page content

--- static/app.js
+++ static/app.js
@@ -1,1 +1,41 @@
-console.log('power overwhelming');
+$(document).ready(function() {
+	console.log('power overwhelming');
+
+	$('#fold').waypoint(function(direction){
+		if (direction == "down") {
+			console.log('triggered down');
+			$('.nav').addClass('sticky');
+		} else {
+			$('.nav').removeClass('sticky');
+			console.log('triggered up');
+		}
+	});
+
+	$('.js--scroll-to-fold').click(function(){
+		$('html, body').animate({scrollTop: $('#fold').offset().top}, 1000);
+	});
+
+/*
+var waypoints = $('#handler-first').waypoint(function(direction) {
+  notify(this.element.id + ' hit 25% from top of window') 
+}, {
+  offset: '25%'
+})
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});

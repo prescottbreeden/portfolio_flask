f31c84d8d0e4126720837ce97430f27b2f150f63
prescottbreeden <prescott.breeden@gmail.com>
2018-07-23 02:44:48 -0700

    created run script, made box grid instead of hex grid

--- app/static/app.js
+++ app/static/app.js
@@ -434,152 +434,33 @@ $(document).ready(function() {
 
 	function loadTech() {
 		const content = document.getElementById('fold');
+		const total = 28;
+		let grid = ` 
+				<section class="tech">
+					<h2 class="tech__header">Some of My Favorite Tech</h2>
+					<ul class="grid clear">
+		`
 
-		content.innerHTML = `
-
-		<section class="tech">
-			<h2 class="tech__header">Some of My Favorite Tech</h2>
-			<ul class="grid clear">
-				<li class="hex-container" >
-					<div class="hexagon img-1"> 
+		for(let i = 1; i <= total; i++) {
+			let box = 'img-cube';
+			if(i === 1 || i === 15) {
+				box = 'img-cube-red';
+			}
+			if (i === 14 || i === 28) {
+				box = 'img-cube-foam'
+			}
+			grid += `
+				<li class="hex-container">
+					<div class="hexagon img-${i}"> 
 						<span class="transparent">flask</span>
 					</div>
+					<div class="${box}"></div>
 				</li>     
-				<li class="hex-container" >
-					<div class="hexagon img-9">
-						<span class="transparent">django</span>
-					</div>
-				</li>     
-				<li class="hex-container" >
-					<div class="hexagon img-10">
-						<span class="transparent">dotnet</span>
-					</div>
-				</li>     
-				<li class="hex-container" >
-					<div class="hexagon img-17">
-						<span class="transparent">nodejs</span>
-					</div>
-				</li>     
-				<li class="hex-container" >
-					<div class="hexagon img-18">
-						<span class="transparent">express</span>
-					</div>
-				</li>     
-				<li class="hex-container" >
-					<div class="hexagon img-19">
-						<span class="transparent">angular</span>
-					</div>
-				</li>     
-				<li class="hex-container" >
-					<div class="hexagon img-27">
-						<span class="transparent">react</span>
-					</div>
-				</li>     
-				<li class="hex-container" >
-					<div class="hexagon img-2" >
-						<span class="transparent">python</span>
-					</div>
-				</li>     
-				<li class="hex-container" >
-					<div class="hexagon img-8">
-						<span class="transparent">csharp</span>
-					</div>
-				</li>     
-				<li class="hex-container" >
-					<div class="hexagon img-11">
-						<span class="transparent">r</span>
-					</div>
-				</li>     
-				<li class="hex-container" >
-					<div class="hexagon img-16">
-						<span class="transparent">sql</span>
-					</div>
-				</li>     
-				<li class="hex-container" >
-					<div class="hexagon img-20">
-						<span class="transparent">mongodb</span>
-					</div>
-				</li>     
-				<li class="hex-container" >
-					<div class="hexagon img-26">
-						<span class="transparent">firebase</span>
-					</div>
-				</li>     
-				<li class="hex-container" >
-					<div class="hexagon img-28">
-						<span class="transparent">sqlite</span>
-					</div>
-				</li>     
-				<li class="hex-container" >
-					<div class="hexagon img-3">
-						<span class="transparent">sass</span>
-					</div>
-				</li>     
-				<li class="hex-container" >
-					<div class="hexagon img-5">
-						<span class="transparent">jquery</span>
-					</div>
-				</li>     
-				<li class="hex-container" >
-					<div class="hexagon img-7">
-						<span class="transparent">bootstrap</span>
-					</div>
-				</li>     
-				<li class="hex-container" >
-					<div class="hexagon img-12">
-						<span class="transparent">postman</span>
-					</div>
-				</li>     
-				<li class="hex-container" >
-					<div class="hexagon img-15">
-						<span class="transparent">git</span>
-					</div>
-				</li>     
-				<li class="hex-container" >
-					<div class="hexagon img-21">
-						<span class="transparent">github</span>
-					</div>
-				</li>     
-				<li class="hex-container" >
-					<div class="hexagon img-25">
-						<span class="transparent">vim</span>
-					</div>
-				</li>     
-				<li class="hex-container" >
-					<div class="hexagon img-4">
-						<span class="transparent">aws</span>
-					</div>
-				</li>     
-				<li class="hex-container" >
-					<div class="hexagon img-6">
-						<span class="transparent">ubuntu</span>
-					</div>
-				</li>     
-				<li class="hex-container" >
-					<div class="hexagon img-13">
-						<span class="transparent">nginx</span>
-					</div>
-				</li>     
-				<li class="hex-container" >
-					<div class="hexagon img-14">
-						<span class="transparent">html5</span>
-					</div>
-				</li>     
-				<li class="hex-container" >
-					<div class="hexagon img-22">
-						<span class="transparent">css3</span>
-					</div>
-				</li>     
-				<li class="hex-container" >
-					<div class="hexagon img-23">
-						<span class="transparent">javascript</span>
-					</div>
-				</li>     
-				<li class="hex-container" >
-					<div class="hexagon img-24">
-						<span class="transparent">vscode</span>
-					</div>
-				</li>     
+
+			`	
+		}
+
+		grid += `
 			</ul>
 			<div class="tech__details">
 				<div class="tech__details--container">
@@ -733,8 +614,9 @@ $(document).ready(function() {
 				</div>
 			</div>
 		</section>
-
-		`;
+		` 
+		
+		content.innerHTML = grid;
 
 	}
 
@@ -828,7 +710,7 @@ $(document).ready(function() {
 		</section>
 
 		`;
-
+		
 	}
 
 
---
+++ run.sh
@@ -0,0 +1,5 @@
+source virtualenv/bin/activate
+export FLASK_APP=server.py
+export FLASK_ENV=development
+export FLASK_DEBUG=1
+flask run --host 10.0.0.115

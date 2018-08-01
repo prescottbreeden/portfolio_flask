69955ddc8ab0dde92cd3dffc2a0b496427f86a61
prescottbreeden <prescott.breeden@gmail.com>
2018-08-01 02:35:53 -0700

    switching over to SES to try and fix contact form bug

--- config.py
+++ config.py
@@ -8,11 +8,11 @@ class Config(object):
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # email server
-    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_SERVER = 'email-smtp.us-west-2.amazonaws.com'
     MAIL_PORT = 465
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
-    MAIL_USERNAME = 'cnorris2882@gmail.com'
-    MAIL_PASSWORD = 'baddog_101'
+    MAIL_USERNAME = 'AKIAIV35GBLZAHPEQICQ'
+    MAIL_PASSWORD = 'AnwYzMGpqidRIYsWHUI82IB9pjWUULaAogowWTMIFIko'
     # administrator list
     ADMINS = ['cnorris2882@gmail.com']

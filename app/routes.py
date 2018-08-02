3a023409a7a3a5f773edd44bd36a019faee1154b
prescottbreeden <prescott.breeden@gmail.com>
2018-08-02 02:51:00 -0700

    removed email entirely, changed layout of contact, removed templates to work around post-deployment editing bug

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,7 @@
 from flask import render_template
-from flask import request
 from app import app
 from app.models import Tech
 import json
-from flask_mail import Message
-from app import mail
-import re
 
 
 @app.route('/')
@@ -20,57 +16,3 @@ def tech(id):
         if t.name == id:
             return json.dumps({'name': t.name, 'blurb': t.blurb})
     return 'query failed'
-
-
-@app.route('/process', methods=['POST'])
-def contact():
-
-    errors = {}
-    name_errors = []
-    email_errors = []
-    message_errors = []
-
-    if len(request.form['name']) < 1:
-        error = 'Name cannot be blank'
-        name_errors.append(error)
-    if len(request.form['email']) < 1:
-        error = 'Email cannot be blank'
-        email_errors.append(error)
-    if not re.match(r"[^@]+@[^@]+\.[^@]+", request.form['email']):
-        error = 'Email must be valid'
-        email_errors.append(error)
-    if len(request.form['message']) < 1:
-        error = 'Message cannot be blank'
-        message_errors.append(error)
-
-    errors = {
-        'name': name_errors,
-        'email': email_errors,
-        'message': message_errors
-    }
-
-    if(errors['name'] or errors['email'] or errors['message']):
-        print(errors)
-        return render_template('contact.html', errs=errors, form=request.form)
-    else:
-        # display success message
-        mail.connect()
-        msg = Message("Contact From Website",
-                      sender='cnorris2882@gmail.com',
-                      recipients=["prescott.breeden@gmail.com"])
-        msg.body = request.form['message']
-        msg.html = (
-
-            "<h2>Contact from Website</h2>"
-            f"<h3>Name: {request.form['name']}</h3>"
-            f"<h3>Email: {request.form['email']}</h3>"
-            f"<p>Message: {request.form['message']}</p>"
-
-         )
-
-        with app.app_context():
-            mail.send(msg)
-
-        msg = request.form['message']
-
-        return render_template('success.html', info=request.form, msg=msg)
--- config.py
+++ config.py
@@ -7,12 +7,3 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # email server
-    MAIL_SERVER = 'email-smtp.us-west-2.amazonaws.com'
-    MAIL_PORT = 465
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL = True
-    MAIL_USERNAME = 'AKIAIV35GBLZAHPEQICQ'
-    MAIL_PASSWORD = 'AnwYzMGpqidRIYsWHUI82IB9pjWUULaAogowWTMIFIko'
-    # administrator list
-    ADMINS = ['cnorris2882@gmail.com']
